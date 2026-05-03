04811c5da447f33519dc11887cfad972ab448da4
takebul <takebulislam@gmail.com>
2026-05-04 01:53:04 +0600

    update better auth url

--- src/components/TopGenerations.jsx
+++ src/components/TopGenerations.jsx
@@ -8,7 +8,7 @@ const TopGenerations = async () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold my-5">Top Generations </h1>
+      <h1 className="text-2xl font-bold my-5">Top Generations</h1>
 
       <div className="grid grid-cols-4 gap-4">
         {topPhotos.map((photo) => (
